ba12ff82ac39799617aa0089718138f4506522e4
Alexander Izumroot <izumroot@deeplay.io>
2023-12-13 23:37:26 +0600

    fix: rename

---
+++ RabbitMQ.Consumer.Net/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using Serializer;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "admin",
+    Password = "89831143406"
+    //VirtualHost = "my-rabbit"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("task_queue",
+    true,
+    false,
+    false,
+    null);
+
+channel.BasicQos(0, 1, false);
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (_, ea) =>
+{
+    var body = ea.Body.ToArray();
+    //var message = Encoding.UTF8.GetString(body);
+    //Console.WriteLine("Received {0}", message);
+
+    var evtDeserialize = Data.DeserializeFromStream<ExtendData>(body);
+    Console.WriteLine(
+        $"Id {evtDeserialize?.Id} | Time {evtDeserialize?.TimeStamp} | Message [{evtDeserialize?.Message}]");
+
+    channel.BasicAck(ea.DeliveryTag, false);
+};
+channel.BasicConsume("task_queue",
+    false,
+    consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ RabbitMQ.Producer.Net/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+
+using Serializer;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "admin",
+    Password = "89831143406"
+    //VirtualHost = "my-rabbit"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("task_queue",
+        true,
+        false,
+        false,
+        null);
+
+    var iterator = 0;
+
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = true;
+
+    while (channel.IsOpen)
+    {
+        var message = $"Message {iterator} | Current data is {DateTime.Now:F}";
+        //var body = Encoding.UTF8.GetBytes(message);
+
+        var dt = new ExtendData(DateTime.Now, message);
+        var evt = Data.SerializeToStream(dt);
+
+        channel.BasicPublish("",
+            "task_queue",
+            properties,
+            evt.ToArray());
+
+        //Console.WriteLine($"Sent [{iterator++}] | Message [{message}]");
+        Console.WriteLine($"Sent [{iterator++}] | Message {message}");
+        Task.Delay(1000).Wait();
+    }
+}
+
+Console.ReadLine();
